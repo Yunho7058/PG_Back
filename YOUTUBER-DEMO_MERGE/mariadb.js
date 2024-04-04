5080e28c24b5a5d0b74587329c4fcf2276088e91
yunho <Yunho7058@github.com>
2024-04-04 21:39:57 +0900

    0401

--- YOUTUBER-DEMO_MERGE/mariadb.js
+++ YOUTUBER-DEMO_MERGE/mariadb.js
@@ -4,20 +4,22 @@ const mysql = require('mysql2');
 // Create the connection to database
 //! 기존에 있던 db 충돌되어 docker에서 port트를 3307로 설정후 추가로 port 번호도 추가해 내가 찾고자하는 db
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 3307,
   user: 'root',
   password: 'root',
   database: 'Youtube',
-  dateStrings: true
+  dateStrings: true,
 });
 
-connection.query('SELECT * FROM `users`', (err, results, fields) => {
-  console.log(err);
-  console.log(results[0]); // 내가 원하는 데이터
-  console.log(fields); //  result 예외
-});
+//컨넥션으로 보내는 sql 매번 이렇게 보내기 불편
+// connection.query('SELECT * FROM `users`', (err, results, fields) => {
+//   console.log(err);
+//   console.log(results[0]); // 내가 원하는 데이터
+//   console.log(fields); //  result 예외
+// });
 
+module.exports = connection;
 // A simple SELECT query
 // try {
 //   const [results, fields] = connection.query('SELECT * FROM `users`');
--- YOUTUBER-DEMO_MERGE/routes/user_old.js
+++ YOUTUBER-DEMO_MERGE/routes/user_old.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const conn = require('../mariadb');
+
 const router = express.Router();
 router.use(express.json()); //json 형태로 사용
-
-//db 연동 -> result 값으로 가지고 놀기
-conn.query('SELECT * FROM `users`', (err, results, fields) => {});
 
 const userInfo = [];
 let id = 1;
@@ -36,60 +33,33 @@ router.post('/login', (req, res) => {
 });
 //회원가입 기능
 router.post('/singup', (req, res) => {
-  const { email, pwd, name, contact } = req.body;
-  if (!email || !pwd || !name || !contact) {
+  const { userId, pwd, name } = req.body;
+  if (!userId || !pwd || !name) {
     res.status(400).json({
       message: `입력값을 확인해주세요.`,
     });
   } else {
-    conn.query(
-      `INSERT INTO users(email, pwd, name,contact) VALUES (?,?,?,?)`,
-      [email, pwd, name, contact],
-      (err, results, fields) => {
-        // results 회원가입이라 따로 없음
-        res.status(201).json({
-          message: `${name}님 환영합니다.`,
-        });
-      }
-    );
-    // userInfo.push({ id: id++, userId, pwd, name });
-    // res.status(201).json({
-    //   message: `${name}님 환영합니다.`,
-    // });
+    userInfo.push({ id: id++, userId, pwd, name });
+    res.status(201).json({
+      message: `${name}님 환영합니다.`,
+    });
   }
 });
 router
   .route('/users')
   .get((req, res) => {
-    let { email } = req.body;
-    conn.query(
-      `SELECT * FROM users WHERE email =?`,
-      email,
-      (err, results, fields) => {
-        if (results.length) {
-          res.status(200).json(results);
-        } else {
-          res.status(404).json({
-            message: '회원정보가 없습니다.',
-          });
-        }
-      }
-    );
-    //! socket hang up 오류 찾기 -> socket(클라이언트와 서버가 주고받는 주머니)
-    // 템플릿 문자열 불가능
-
-    //? old 코드
-    //const user = userInfo.filter((el) => el.userId === userId);
-    // if (user.length === 0) {
-    //   res.status(404).json({
-    //     message: '회원정보가 없습니다.',
-    //   });
-    // } else {
-    //   res.status(200).json({
-    //     userId: user[0].userId,
-    //     name: user[0].name,
-    //   });
-    // }
+    let { userId } = req.body;
+    const user = userInfo.filter((el) => el.userId === userId);
+    if (user.length === 0) {
+      res.status(404).json({
+        message: '회원정보가 없습니다.',
+      });
+    } else {
+      res.status(200).json({
+        userId: user[0].userId,
+        name: user[0].name,
+      });
+    }
   })
   .delete((req, res) => {
     let { id } = req.params;
